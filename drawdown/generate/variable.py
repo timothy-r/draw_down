1cb87a29822eccc22365320b0eb1f1081a018d91
timothy-r <tim.rodger@gmail.com>
2025-10-03 23:03:19 +0100

    starts to implement variable

--- drawdown/generate/variable.py
+++ drawdown/generate/variable.py
@@ -1,3 +1,4 @@
+import random
 class Variable:
     """
         produces a variable value that fluctuates beween min & max
@@ -6,6 +7,8 @@ class Variable:
 
     def __init__(self, average:float, min:float, max:float, period:int) -> None:
 
+        # assert that min < average < max
+
         self._average = average
         self._min = min
         self._max = max
@@ -13,6 +16,33 @@ class Variable:
 
         # keep a record of generated values
         self._values = []
+        diff = max - min
+        step = diff / (period*2)
+        self._population = []
+        # weights prefer the central value
+        # the middle value should be 0.5
+        # calculate others spreading out lower & higher
+        self._weights = []
+
+        item = min
+
+        for i in range(0, period*2):
+
+            self._population.append(item)
+            item = round(item+step,2)
+            self._weights.append(1/(period*2))
+
+        self._population.append(max)
+        self._weights.append(1/(period*2))
+
+        print(f"_population {self._population}")
+        print(f"_weights {self._weights}")
 
     def next(self) -> float:
-        pass
+        return random.choices(
+            population=self._population,
+            weights=self._weights,
+            k=1
+        )[0]
+
+        return random.uniform(self._min, self._max)
--- tests/variable_test.py
+++ tests/variable_test.py
@@ -1,4 +1,7 @@
 import unittest
+import logging
+import statistics
+
 from drawdown.generate.variable import Variable
 
 class VariableTest(unittest.TestCase):
@@ -13,4 +16,37 @@ class VariableTest(unittest.TestCase):
         var = Variable(average=average, min=min, max=max, period=period)
 
         next = var.next()
-        self.assertGreaterEqual(min, next)
+        # print(f"test_min {next}")
+        self.assertGreaterEqual(next, min)
+
+    def test_max(self):
+
+        average = 5.0
+        min = 1.0
+        max = 10.0
+        period = 5
+
+        var = Variable(average=average, min=min, max=max, period=period)
+
+        next = var.next()
+        self.assertLessEqual(next, max)
+
+    def test_average(self):
+
+        min = 1.0
+        max = 10.0
+        average = round(statistics.mean([min,max]), 1)
+        period = 5
+
+        var = Variable(average=average, min=min, max=max, period=period)
+
+        counter = 0
+        vars = []
+
+        while(counter < period):
+            vars.append(var.next())
+            counter +=1
+
+        actual_average = round(statistics.mean(vars), 1)
+
+        self.assertEqual(actual_average, average)
